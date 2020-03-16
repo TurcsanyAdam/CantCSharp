2d9a77edea53bc5b2355a7dfbdcb84cdab355c11
feher.attila96@gmail.com <feher.attila96@gmail.com>
2020-03-16 11:31:08 +0100

    Created the Answer class

---
+++ CantCSharp/Models/Answer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CantCSharp.Models
+{
+    public class Answer
+    {
+        int Id { get; }
+        string TheAnswer { get; set; }
+
+        bool IsSolution { get; set; }
+
+        public Answer(int id, string theAnswer)
+        {
+            Id = id;
+            TheAnswer = theAnswer;
+            IsSolution = false;
+
+        }
+
+    }
+}
