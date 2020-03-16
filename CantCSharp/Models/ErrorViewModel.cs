6c18afe38065f1768fc44d605ee77c07c3e9eaac
Ádám Turcsány <turcsanyadam@gmail.com>
2020-03-16 10:24:17 +0100

    Initialize project

---
+++ CantCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CantCSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
