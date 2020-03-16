e3cbd856e928320096898511217d20cf83fe28ad
Csondika <csondikaa@gmail.com>
2020-03-16 11:30:15 +0100

    Create IAnswer and IQuestion.

---
+++ CantCSharp/Models/IQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CantCSharp.Models
+{
+    interface IQuestion
+    {
+        void AddAnswer(IAnswer answer);
+        void MarkAsAnswered();
+        void Close();
+    }
+}
