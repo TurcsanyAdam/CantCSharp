79dfc8a511c5b56137a3fe93814dbc893e1e1afb
Ádám Turcsány <turcsanyadam@gmail.com>
2020-03-16 14:16:22 +0100

    Questions from csv

---
+++ CantCSharp/Models/CsvLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CantCSharp.Models
+{
+    public class CsvLoader
+    {
+        public static List<QuestionModel> LoadData()
+        {
+            List<QuestionModel> questions = new List<QuestionModel>();
+            using (var reader = new StreamReader(@"C:\Users\Turi\source\repos\CantCSharp\CantCSharp\wwwroot\csv\questions.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    questions.Add(new QuestionModel(Convert.ToInt32(values[0]), values[1], values[2]));
+                }
+                   
+            }
+            return questions;
+        }
+    }
+}
