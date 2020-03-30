4e9886bc5b845c2a92ab268d096d9b3bfd6e9d9c
Ádám Turcsány <turcsanyadam@gmail.com>
2020-03-30 13:21:14 +0200

    Environment variables in databaseloader

--- CantCSharp/Models/DataBaseLoader.cs
+++ CantCSharp/Models/DataBaseLoader.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Npgsql;
-using 
 
 namespace CantCSharp.Models
 {
     public class DataBaseLoader : IDataLoad
     {
+        private static readonly string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+        private static readonly string dbUser = Environment.GetEnvironmentVariable("DB_USER");
+        private static readonly string dbPass = Environment.GetEnvironmentVariable("DB_PASS");
+        private static readonly string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+        public static readonly string connectingString = $"Host={dbHost};Username={dbUser};Password={dbPass};Database={dbName}";
         public List<QuestionModel> QuestionList { get; set; } = new List<QuestionModel>();
 
         public void AddQuestion(string message, string title, string user)
@@ -16,7 +20,7 @@ namespace CantCSharp.Models
             QuestionList.Add(new QuestionModel(QuestionList.Count + 1, message, title, user));
         }
 
-        List<QuestionModel> LoadData(string connectingString,string select)
+        List<QuestionModel> LoadData(string select)
         { 
             using(NpgsqlConnection connection = new NpgsqlConnection(connectingString))
             {
