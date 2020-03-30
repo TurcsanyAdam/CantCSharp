788ce011b209ce7ad80b720f2d0b9eba3e9e6a91
Csondika <csondikaa@gmail.com>
2020-03-30 13:38:13 +0200

    Add empty SortByVote method. Upgrade index and allquestions visuals a little to match given sorting task.

--- CantCSharp/Models/DataBaseLoader.cs
+++ CantCSharp/Models/DataBaseLoader.cs
@@ -3,35 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Npgsql;
-using 
 
 namespace CantCSharp.Models
 {
-    public class DataBaseLoader : IDataLoad
-    {
-        public List<QuestionModel> QuestionList { get; set; } = new List<QuestionModel>();
+    //public class DataBaseLoader : IDataLoad
+    //{
+    //    public List<QuestionModel> QuestionList { get; set; } = new List<QuestionModel>();
 
-        public void AddQuestion(string message, string title, string user)
-        {
-            QuestionList.Add(new QuestionModel(QuestionList.Count + 1, message, title, user));
-        }
+    //    public void AddQuestion(string message, string title, string user)
+    //    {
+    //        QuestionList.Add(new QuestionModel(QuestionList.Count + 1, message, title, user));
+    //    }
 
-        List<QuestionModel> LoadData(string connectingString,string select)
-        { 
-            using(NpgsqlConnection connection = new NpgsqlConnection(connectingString))
-            {
-                connection.Open();
-                NpgsqlCommand command = new NpgsqlCommand(select, connection);
-                NpgsqlDataReader datareader = command.ExecuteReader();
+    //    List<QuestionModel> LoadData(string connectingString,string select)
+    //    { 
+    //        using(NpgsqlConnection connection = new NpgsqlConnection(connectingString))
+    //        {
+    //            connection.Open();
+    //            NpgsqlCommand command = new NpgsqlCommand(select, connection);
+    //            NpgsqlDataReader datareader = command.ExecuteReader();
 
-                while (datareader.Read())
-                {
+    //            while (datareader.Read())
+    //            {
 
-                }
+    //            }
 
-            }
-        }
+    //        }
+    //    }
 
 
-    }
+    //}
 }
