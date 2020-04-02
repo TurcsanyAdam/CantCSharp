66d60871d344e6efe37c4c93f977f733f5800c7a
feher.attila96@gmail.com <feher.attila96@gmail.com>
2020-04-02 13:04:02 +0200

    the ID now id in the QuestionDetails

--- CantCSharp/Controllers/HomeController.cs
+++ CantCSharp/Controllers/HomeController.cs
@@ -42,9 +42,9 @@ namespace CantCSharp.Controllers
         }
 
         [HttpGet]
-        public IActionResult QuestionDetails(int ID)
+        public IActionResult QuestionDetails(int id)
         {
-            QuestionModel questionModel = _loader.GetDataList($"SELECT * FROM question WHERE question_id = {ID};")[0];
+            QuestionModel questionModel = _loader.GetDataList($"SELECT * FROM question WHERE question_id = {id};")[0];
             questionModel.ViewNumber++;
             return View(questionModel);
         }
@@ -96,7 +96,7 @@ namespace CantCSharp.Controllers
         } 
 
         [HttpPost]
-        public IActionResult NewAnswer([FromForm(Name = "answer")] string answer, [FromForm(Name = "username")] string username,[FromForm(Name ="Image")] string imagesource,
+        public void NewAnswer([FromForm(Name = "answer")] string answer, [FromForm(Name = "username")] string username,[FromForm(Name ="Image")] string imagesource,
            int id, [FromForm(Name = "Link")]string link)
         {
             if(imagesource != null)
@@ -271,7 +271,7 @@ namespace CantCSharp.Controllers
         {
             _loader.UpdateDataRow($"Update askmate_question_comment SET comment_message = '{editedcomment}' Where comment_id = {Convert.ToString(EditedCommentID)} and question_id = {Convert.ToString(EditedCommentQuestionID)}");
             var questionModel = _loader.GetDataList("SELECT * FROM question;");
-            Response.Redirect($"QuestionDetails/{EditedCommentID}");
+            Response.Redirect($"QuestionDetails/{EditedCommentQuestionID}");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
