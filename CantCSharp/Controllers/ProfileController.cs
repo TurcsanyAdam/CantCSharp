f3f2f0e47b26f1ef80fba51743df5139e1834187
feher.attila96@gmail.com <feher.attila96@gmail.com>
2020-04-13 13:30:24 +0200

    Trying to implement the Cookies.

--- CantCSharp/Controllers/ProfileController.cs
+++ CantCSharp/Controllers/ProfileController.cs
@@ -25,46 +25,6 @@ namespace CantCSharp.Controllers
             return View();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> LoginAsync([FromForm]string email, [FromForm] string password)
-        {
-
-            //here comes the Loginformation storing sql querry;
-            var claims = new List<Claim> { new Claim(ClaimTypes.Email, email) };
-
-            var claimsIdentity = new ClaimsIdentity(
-                claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-            var authProperties = new AuthenticationProperties
-            {
-                //AllowRefresh = <bool>,
-                // Refreshing the authentication session should be allowed.
-
-                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                // The time at which the authentication ticket expires. A 
-                // value set here overrides the ExpireTimeSpan option of 
-                // CookieAuthenticationOptions set with AddCookie.
-
-                //IsPersistent = true,
-                // Whether the authentication session is persisted across 
-                // multiple requests. When used with cookies, controls
-                // whether the cookie's lifetime is absolute (matching the
-                // lifetime of the authentication ticket) or session-based.
-
-                //IssuedUtc = <DateTimeOffset>,
-                // The time at which the authentication ticket was issued.
-
-                //RedirectUri = <string>
-                // The full path or absolute URI to be used as an http 
-                // redirect response value.
-            };
-
-            await HttpContext.SignInAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(claimsIdentity),
-                authProperties);
-
-            return RedirectToAction("Index", "Home");
-        }
+        
     }
 }
