7a2a739d046c6f2899ecd634fcfb2186a9ecbaaf
Ádám Turcsány <turcsanyadam@gmail.com>
2020-04-13 11:19:18 +0200

    User class, profile controller

---
+++ CantCSharp/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CantCSharp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CantCSharp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IDataLoad _loader;
+        public ProfileController(IDataLoad loader)
+        {
+            _loader = loader;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CantCSharp/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CantCSharp.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string email, string password)
+        {
+            UserName = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
