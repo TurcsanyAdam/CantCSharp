b15e3d32ba90959c12a3d69e8748129266f74c9a
Csondika <csondikaa@gmail.com>
2020-04-16 11:09:18 +0200

    Finish functionality and design of ProfileDetails page.

--- CantCSharp/Controllers/ProfileController.cs
+++ CantCSharp/Controllers/ProfileController.cs
@@ -26,11 +26,16 @@ namespace CantCSharp.Controllers
             var user = HttpContext.User;
             var claim = user.Claims.First(c => c.Type == ClaimTypes.Email);
             var email = claim.Value;
+
             User searchedUser = _loader.GetUserList($"Select * FROM users WHERE email = '{email}'")[0];
+            List<QuestionModel> searchedQuestionsList = _loader.GetUserQuestions(searchedUser, "SELECT q.* FROM question q " +
+                                                                        "LEFT JOIN answer a ON q.question_id = a.question_id " +
+                                                                        $"WHERE q.userid = {searchedUser.UserId} " +
+                                                                        $"OR a.userid = {searchedUser.UserId} " +
+                                                                        "GROUP BY q.question_id; ");
 
-            
-
-            return View(searchedUser);
+            ProfileDetailsModel profileDetailModel = new ProfileDetailsModel(searchedUser, searchedQuestionsList);
+            return View(profileDetailModel);
         }
     }
 }
---
+++ CantCSharp/Models/ProfileDetailsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CantCSharp.Models
+{
+    public class ProfileDetailsModel
+    {
+        public User User { get; private set; }
+        public List<QuestionModel> QuestionsList { get; private set; }
+
+        public ProfileDetailsModel(User user, List<QuestionModel> questionsList)
+        {
+            User = user;
+            QuestionsList = questionsList;
+        }
+    }
+}
